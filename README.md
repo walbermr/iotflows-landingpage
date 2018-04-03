da8f4948b222054c1b9d3acd547d6f4f009af5d0
walbermr <wmr@cin.ufpe.br>
2018-04-03 15:47:02 -0300

    Added fres_install and updated README

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
-
-Things you may want to cover:
+You can use $ fresh-install.sh if using a fresh system install.
 
 * Ruby version
 
+        ruby 2.3.1
+
 * System dependencies
+
+        packages: nodejs ruby ruby-dev make zlib1g-dev libicu-dev libpq-dev build-essential git cmake yarn tmux go postgresql postgresql-contrib libpq-dev
+        go packages: overmind
+
+        Overmind is usefull to execute procfile.
 
 * Configuration
 
---
+++ fresh-install.sh
@@ -0,0 +1,21 @@
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update
+sudo apt-get install -y nodejs ruby ruby-dev make zlib1g-dev libicu-dev libpq-dev build-essential git cmake yarn golang postgresql postgresql-contrib libpq-dev
+
+bundle install --with=GROUP[development]
+
+mkdir ~/.go
+echo "GOPATH=$HOME/.go" >> ~/.bashrc
+echo "export GOPATH" >> ~/.bashrc
+echo "PATH=\$PATH:\$GOPATH/bin # Add GOPATH/bin to PATH for scripting" >> ~/.bashrc
+source ~/.bashrc
+go get -u -f github.com/DarthSim/overmind
+
+yarn
+
+rails db:create
+rails db:migrate
+
+echo "Environment is ready to go..."
