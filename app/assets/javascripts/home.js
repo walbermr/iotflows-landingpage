a9bc9757a5609660d3e8742ce67307b258bbb92a
walbermr <wmr@cin.ufpe.br>
2018-04-12 16:47:48 -0300

    Showing project members, added footer and header in homepage

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,15 @@
+function myMap()
+{
+  myCenter=new google.maps.LatLng(-8.0556681,-34.951578);
+  var mapOptions= {
+    center:myCenter,
+    zoom:12, scrollwheel: false, draggable: false,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: myCenter,
+  });
+  marker.setMap(map);
+}
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,11 @@
 class HomeController < ActionController::Base
-  def home; end
+  def home;
+    @doctors = filter_users_by_regex(/D+/)  
+    @masters = filter_users_by_regex(/M+/)
+    @bachelors = filter_users_by_regex(/U+/)
+  end
+
+  def filter_users_by_regex(regex)
+    return User.all.select{|i| i.occupation.match(regex)}
+  end
 end
