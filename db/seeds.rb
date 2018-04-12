f1d552ccceb89f7f9f57c27967e8b8f991d1d8eb
walbermr <wmr@cin.ufpe.br>
2018-04-12 16:48:53 -0300

    Added seeds to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(email:'wmr@cin.ufpe.br',  password:'wmr123', username:'walbermr',
+ occupation:'UROP Computer Engineering', name:'Walber de Macedo Rodrigues')
+User.create!(email:'irbp@cin.ufpe.br',  password:'irbp123', username:'irbp',
+ occupation:'UROP Computer Engineering',  name:'Ítalo Rodrigo Barbosa Paulino')
+User.create!(email:'abl3@cin.ufpe.br',  password:'lapprand123', username:'lapprand',
+ occupation:'UROP Computer Science',  name:'Arthur Barros Lapprand')
